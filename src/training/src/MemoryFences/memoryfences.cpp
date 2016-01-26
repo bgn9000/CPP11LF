39c7b9b44a98056c673bad71998ad5b5abbc16fe
bgn9000 <pbourgeon@gmail.com>
2016-01-26 09:02:50 +0800

    Fixed memory fence example

--- src/training/src/MemoryFences/memoryfences.cpp
+++ src/training/src/MemoryFences/memoryfences.cpp
@@ -19,8 +19,8 @@ std::string thread_siblings_list()
 struct Data
 {
     int nb = 0;
-    bool stop = false;
-    std::atomic<int> start{0};
+    std::atomic<bool> start{false};
+    std::atomic<bool> stop{false};
     
     int x = 0;
     int y = 0;
@@ -34,85 +34,77 @@ using millis = duration<double, std::milli>;
 
 void threaded_function1(Data& data, int max)
 {
-    ++data.start;
-    while (data.start < 2) ;
+    while (!data.start) ;
     high_resolution_clock::time_point start = high_resolution_clock::now();
-    while (1)
+    while (++data.nb < max)
     {
-        assert(data.x == 0);
-        assert(data.y == 0);
-        assert(data.z == 0);
-        data.x = 1;
-        data.y = 2;
-        data.z = 3;
-        data.flag.store(1, std::memory_order_relaxed);
-        while (data.flag.load(std::memory_order_relaxed) == 1) ;
-        if (++data.nb > max) { data.stop = true; break; }
+        int flag = data.flag.load(std::memory_order_relaxed);
+        if (flag == 0)
+        {
+            ++data.x;
+            ++data.y;
+            ++data.z;
+            std::atomic_thread_fence(std::memory_order_release);
+            data.flag.store(1, std::memory_order_relaxed);
+        }
     }
     high_resolution_clock::time_point end = high_resolution_clock::now();
     data.time_span = duration_cast<millis>(end - start).count();
-    
-    data.x = 1;
-    data.y = 2;
-    data.z = 3;
-    data.flag.store(1, std::memory_order_relaxed);
+    data.stop = true;
 }
 
 void threaded_function2(Data& data)
 {
-    ++data.start;
+    auto x_prev = 0, y_prev = 0, z_prev = 0;
+    data.start = true;
     while (!data.stop)
     {
-        while (data.flag.load(std::memory_order_relaxed) == 0) ;
-        std::atomic_thread_fence(std::memory_order_acquire);
-        assert(data.x == 1);
-        assert(data.y == 2);
-        assert(data.z == 3);
-        data.x = data.y = data.z = 0;
-        std::atomic_thread_fence(std::memory_order_release);
-        data.flag.store(0, std::memory_order_relaxed);
+        int flag = data.flag.load(std::memory_order_relaxed);
+        if (flag != 0)
+        {
+            std::atomic_thread_fence(std::memory_order_acquire);
+            assert(data.x == ++x_prev);
+            assert(data.y == ++y_prev);
+            assert(data.z == ++z_prev);
+            std::atomic_thread_fence(std::memory_order_release);
+            data.flag.store(0, std::memory_order_relaxed);
+        }    
     }
 }
 
 void threaded_function1b(Data& data, int max)
 {
-    ++data.start;
-    while (data.start < 2) ;
+    while (!data.start) ;
     high_resolution_clock::time_point start = high_resolution_clock::now();
-    while (1)
+    while (++data.nb < max)
     {
-        assert(data.x == 0);
-        assert(data.y == 0);
-        assert(data.z == 0);
-        data.x = 1;
-        data.y = 2;
-        data.z = 3;
-        data.flag = 1;
-        while (data.flag == 1) ;
-        if (++data.nb > max) { data.stop = true; break; }
+        if (data.flag == 0)
+        {
+            ++data.x;
+            ++data.y;
+            ++data.z;
+            data.flag = 1;
+        }
     }
     high_resolution_clock::time_point end = high_resolution_clock::now();
     data.time_span = duration_cast<millis>(end - start).count();
-    
-    data.x = 1;
-    data.y = 2;
-    data.z = 3;
-    data.flag = 1;
+    data.stop = true;
 }
 
 void threaded_function2b(Data& data)
 {
-    ++data.start;
+    auto x_prev = 0, y_prev = 0, z_prev = 0;
+    data.start = true;
     while (!data.stop)
     {
-        while (data.flag == 0) ;
-        assert(data.x == 1);
-        assert(data.y == 2);
-        assert(data.z == 3);
-        data.x = data.y = data.z = 0;
-        data.flag = 0;
+        if (data.flag != 0)
+        {
+            assert(data.x == ++x_prev);
+            assert(data.y == ++y_prev);
+            assert(data.z == ++z_prev);
+            data.flag = 0;
+        }
     }
-    
 }
 
 int main()
@@ -131,7 +123,7 @@ int main()
     cpu_set_t cpuset;
     
     Data data1;
-    std::thread t1(threaded_function1, std::ref(data1), 30'000'000);
+    std::thread t1(threaded_function1, std::ref(data1), 300'000'000);
     CPU_ZERO(&cpuset);
     CPU_SET(core1, &cpuset);
     rc = pthread_setaffinity_np(t1.native_handle(), sizeof(cpu_set_t), &cpuset);
@@ -146,13 +138,11 @@ int main()
     std::this_thread::sleep_for(std::chrono::seconds(10));
     
     Data data2;
-//    std::thread t1b(threaded_function1b, std::ref(data2), 30'000'000);
-    std::thread t1b(threaded_function1, std::ref(data2), 30'000'000);
+    std::thread t1b(threaded_function1b, std::ref(data2), 300'000'000);
     CPU_ZERO(&cpuset);
     CPU_SET(core1, &cpuset);
     rc = pthread_setaffinity_np(t1b.native_handle(), sizeof(cpu_set_t), &cpuset);
-//    std::thread t2b(threaded_function2b, std::ref(data2));
-    std::thread t2b(threaded_function2, std::ref(data2));
+    std::thread t2b(threaded_function2b, std::ref(data2));
     CPU_ZERO(&cpuset);
     CPU_SET(core2, &cpuset);
     rc = pthread_setaffinity_np(t2b.native_handle(), sizeof(cpu_set_t), &cpuset);
