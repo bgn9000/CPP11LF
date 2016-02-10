a3a43cf23cd095c235c2639d1fa4b266903c098a
bgn9000 <pbourgeon@gmail.com>
2016-02-10 15:23:59 +0800

    Add demangle output

--- src/training/src/TypeDeduction/decltype.cpp
+++ src/training/src/TypeDeduction/decltype.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+
 #include <typeinfo>
+#include <cxxabi.h>
 
 double onefunction();
 double secondfunction(double x) { return x*x; }
@@ -16,21 +18,23 @@ auto wrapper(F func, X x) -> decltype(func(x))
 
 int main()
 {
+    auto status = 0;
+    
     // auto is more convenient than decltype (used to get return type if not statically known)
     decltype(onefunction()) f1;
-    std::cout << "type is " << typeid(f1).name() << std::endl; // d
+    std::cout << "type is " << abi::__cxa_demangle(typeid(f1).name(), 0, 0, &status) << std::endl;
     
     decltype(onefunction) f1b;
-    std::cout << "type is " << typeid(f1b).name() << std::endl; // FdvE
+    std::cout << "type is " << abi::__cxa_demangle(typeid(f1b).name(), 0, 0, &status) << std::endl;
     
     decltype(secondfunction) f2;
-    std::cout << "type is " << typeid(f2).name() << std::endl; // FddE
+    std::cout << "type is " << abi::__cxa_demangle(typeid(f2).name(), 0, 0, &status) << std::endl;
     
-    std::cout << "type is " << typeid(toto).name() << std::endl;
+    std::cout << "type is " << abi::__cxa_demangle(typeid(toto).name(), 0, 0, &status) << std::endl;
     
     // return type
     auto res = wrapper(secondfunction, 2);
-    std::cout << "type is " << typeid(res).name() << " and res=" << res << std::endl;
+    std::cout << "type is " << abi::__cxa_demangle(typeid(res).name(), 0, 0, &status) << " and res=" << res << std::endl;
     
     return 0;
 }
