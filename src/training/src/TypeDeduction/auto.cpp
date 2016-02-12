b58fd0578f5e5d3e384f264162023e2471757d73
bgn9000 <pbourgeon@gmail.com>
2016-02-12 21:46:55 +0800

    Add auto example (auto is a compilation time type deduction but nothing related to reference or pointer or constness...)

--- src/training/src/TypeDeduction/auto.cpp
+++ src/training/src/TypeDeduction/auto.cpp
@@ -12,13 +12,20 @@ void printAll(const std::vector<int>& v)
 int main()
 {
     auto v1 = std::vector<int>();
-    for (int cpt = 0; cpt < 5; ++cpt)
+    for (auto cpt = 0; cpt < 5; ++cpt)
     {
         v1.push_back(cpt);
     }
     auto v2 = v1;
     printAll(v1);
     printAll(v2);
+    
+    int i = 0;
+    int& j = i;
+    auto k = j; // auto is an int not a int&
+    std::cout << "i(int)=" << i << " j(int &i)=" << j << " k(auto?)=" << k << std::endl;
+    ++i;
+    std::cout << "i(int)=" << i << " j(int &i)=" << j << " k(auto?)=" << k << std::endl;
     return 0;
 }
 
